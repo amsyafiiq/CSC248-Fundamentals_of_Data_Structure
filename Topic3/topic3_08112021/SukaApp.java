969b24ea89bd2f738565cebd93a8e273e31ffd23
amsyafiiq <81912918+amsyafiiq@users.noreply.github.com>
2021-11-11 11:38:32 +0800

    Update SukaApp.java

--- Topic3/topic3_08112021/SukaApp.java
+++ Topic3/topic3_08112021/SukaApp.java
@@ -6,20 +6,20 @@ public class SukaApp {
     public static void main(String[] args) {
         Node<String> head, current = null;
 
-        Node<String> node = new Node<>("A");
-        head = node; // head = tail = node = "A"
+        Node<String> node = new Node<>("A");  // node = "A", A
+        head = node;                          // head = "A"
 
-        node = new Node<>("K"); // node = "K"
-        node.next = head;       // node.next = "A"
-        head = node;            // head = "K"
+        node = new Node<>("K");               // node = "K", K A
+        node.next = head;                     // node.next = "A"
+        head = node;                          // head = "K"
 
-        node = new Node<>("S"); // node = "S"
-        node.next = head;       // node.next = "K"
-        head = node;            // head = "S"
+        node = new Node<>("S");               // node = "S", S K A
+        node.next = head;                     // node.next = "K"
+        head = node;                          // head = "S"
 
-        node = new Node<>("U"); // node = "U"
-        node.next = head.next;  // node.next = "K"
-        head.next = node;       // head.next = "U"
+        node = new Node<>("U");               // node = "U", S U K A
+        node.next = head.next;                // node.next = "K"
+        head.next = node;                     // head.next = "U"
 
         current = head;
         System.out.println("Output:-");
