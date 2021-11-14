fabbb97f9d754caa6a1847320fa7dcaaa16b031f
amsyafiiq <81912918+amsyafiiq@users.noreply.github.com>
2021-11-14 21:08:55 +0800

    New Tutorial

---
+++ Topic3/topic3_10112021/LinkedListApp.java
@@ -0,0 +1,50 @@
+/**
+ * LinkedListApp
+ */
+//import java.util.LinkedList;
+import java.util.Scanner;
+
+public class LinkedListApp {
+    public static void main(String[] args){
+        Scanner inNum = new Scanner(System.in);
+
+        // Declare an Integer LinkedList named intList
+        LinkedList<Integer> intList = new LinkedList<Integer>();
+
+        // Insert 5 integer value into the LinkedList
+        System.out.println("Enter 5 Number:-");
+        for (int i = 0; i < 5; i++) {
+            System.out.print((i + 1) + ". ");
+            intList.addLast(inNum.nextInt());
+        }
+        inNum.close();
+
+        // Insert Number 10 at the Front
+        intList.addFirst(10);
+
+        // Insert number 3 at the back
+        intList.addLast(3);
+
+        // Remove the first element
+        intList.removeFirst();
+
+        // Display the content of LinkedList, calculate average and sum of even value
+        Integer value = intList.getFirst();
+        System.out.println("Value in Linked List: ");
+        double avg, total = 0;
+        int i = 0, sum = 0;
+        while(value != null) {
+            System.out.print(value + " ");
+            total += value;
+
+            if(value % 2 == 0)
+                sum += value;
+
+            value = intList.getNext(); i++;
+        }
+        avg = total / i;
+        System.out.println("\nAverage Value: " + avg);
+        System.out.println("Sum Value: " + sum);
+
+    }
+}
---
+++ Topic3/topic3_10112021/README.md
@@ -0,0 +1,13 @@
+# Topic 3 Class Exercise | 10 November 2021
+
+## Exercise 1
+Write a main application that will:-
+
+* Declare an `Integer` `LinkedList` named `intList`.
+* Insert 5 `Integer` value into the list.
+* Insert number `10` at the front.
+* Insert number `3` at the back.
+* Remove the first element.
+* Display the content of the `LinkedList`.
+* Calculate and display the average value of the content in `LinkedList`.
+* Display the sum value of even number.
