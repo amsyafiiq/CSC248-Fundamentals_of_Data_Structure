2f539fb59a411754786ce0e5d0a6a4a374852f9e
amsyafiiq <81912918+amsyafiiq@users.noreply.github.com>
2021-11-18 19:47:49 +0800

    Added misc folder

---
+++ misc/universal_LLnNode/Node.java
@@ -0,0 +1,15 @@
+package universal_LLnNode;
+
+/***************************************************************************
+ * Programmer: Roslan Sadjirin, UiTM (Raub, Pahang)
+ * Date: 2020-11-03
+ * Topic 3 - Node Manipulation / LinkedList
+ ***************************************************************************/
+public class Node<E> {
+	public E element;
+	public Node<E> next;
+	
+	public Node(E e) { 
+		element = e; 
+	}
+}
